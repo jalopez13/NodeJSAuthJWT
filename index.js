e361da559538b8613b548c6290c529a082bb8b7b
Javier Lopez <javier.lopez@tmz.com>
2019-08-01 17:05:15 -0700

    Added gitignore and removed node_modules.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const PORT = 5000;
 const app = express();
 const authRoute = require('./routes/auth');
+
+// DB Connect
+mongoose.connect('mongodb://localhost:27017/nodejsauthjwt', { useNewUrlParser: true });
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connected and ready to rock...')
+});
 
 // middleware
 app.use(bodyParser.json());
